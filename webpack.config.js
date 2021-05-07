7b3ba9046990679ee90fdbccd1d0b33452c6863e
minsun <yamyam@pentabreed.com>
2021-05-07 14:46:14 +0900

    copy-webpack-plugin 버전 오류로 에러가 나서 다시 설정 후 push 적용됨

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const apiMocker = require("connect-api-mocker");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+
 const mode = process.env.NODE_ENV || "development";
 
 module.exports = {
-  // TODO: 환경변수 NODE_ENV에 따라 development나 production 값을 설정하세요 
   mode,
   entry: {
     main: "./src/app.js"
@@ -21,6 +22,9 @@ module.exports = {
   devServer: {
     overlay: true,
     stats: "errors-only",
+    // before: (app, server, compiler) => {
+    //   app.use(apiMocker('/api', 'mocks/api'));
+    // },
     proxy: {
       "/api": "http://localhost:8081"
     },
@@ -31,7 +35,7 @@ module.exports = {
       {
         test: /\.(scss|css)$/,
         use: [
-          process.env.NODE_ENV === "production"
+          mode === "production"
             ? MiniCssExtractPlugin.loader // 프로덕션 환경
             : "style-loader", // 개발 환경
           "css-loader",
@@ -61,45 +65,44 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+        env: mode === "development" ? "(개발용)" : ""
       },
       minify:
-        process.env.NODE_ENV === "production"
+        mode === "production"
           ? {
               collapseWhitespace: true, // 빈칸 제거
               removeComments: true // 주석 제거
             }
           : false,
-      hash: process.env.NODE_ENV === "production"
+      hash: mode === "production"
     }),
     new CleanWebpackPlugin(),
-    ...(process.env.NODE_ENV === "production"
-      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
-      : []),
     new CopyPlugin([
       {
         from: "./node_modules/axios/dist/axios.min.js",
-        to: "./axios.min.js", // 목적지 파일에 들어간다
-      },
+        to: "./axios.min.js" // 목적지 파일에 들어간다
+      }
     ]),
+    ...(mode === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : [])
   ],
-  // TODO: 여기에 최적화 설정을 구성하세요 
   optimization: {
-    minimizer: 
-      mode === "production" 
-      ? [
-          new OptimizeCSSAssetsPlugin(),
-          new TerserPlugin({
-            terserOptions: {
-              compress: {
-                drop_console: true, // 콘솔 로그를 제거한다
-              },
-            },
-          }),
-        ]
-      : [],
+    minimizer:
+      mode === "production"
+        ? [
+            new OptimizeCSSAssetsPlugin(),
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true // 콘솔 로그를 제거한다
+                }
+              }
+            })
+          ]
+        : []
   },
   externals: {
-    axios: 'axios'
+    axios: "axios"
   }
 };
